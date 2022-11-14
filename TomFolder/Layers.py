cc5f7d936ec9de4181bfffd8085efef4f69110fb
Tom Cronin <tom100cronin@gmail.com>
2022-11-14 16:54:50 +0000

    Not happy but have a working single layer with ThresholdLogicUnit.py

--- TomFolder/Layers.py
+++ TomFolder/Layers.py
@@ -1,4 +1,8 @@
+import math
+
 from Perceptron import Perceptron
+
+import numpy as np
 
 class Layer():
 
@@ -40,4 +44,43 @@ class Layer():
             output.append(neuron.fit_predict(Data, Labels))
         return output
 
+
+class DenseLayer():
+    def __init__(self, n_nuerons, n_inputs):
+        print(n_nuerons, n_inputs)
+        self.weights = 0.5 * np.random.randn(n_inputs, n_nuerons)
+        self.biases = np.zeros(n_nuerons)
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Relu:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class soft_max():
+    def forward(self, inputs):
+        exp = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        self.output = exp / np.sum(exp, axis=1, keepdims=True)
+        self.preds = []
+        for pred in self.output:
+            self.preds.append(max(pred))
+class loss_function():
+
+    def conver_labels(self, labels):
+        converted = []
+        for label in labels:
+            if 'no' in label.lower():
+                converted.append(0)
+            else:
+                converted.append(1)
+        return converted
+    def loss(self, preds, labels):
+        lost =0
+        labels = self.conver_labels(labels)
+        for index in range(len(labels)):
+            lost += math.log(preds[index]) * labels[0]
+
+
+        return lost
 
